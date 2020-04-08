87b7de12ecad87e798e36c9049875bd39f57d8af
Nat! <nat-tv@mulle-kybernetik.com>
2020-04-08 18:33:12 +0200

    fix header removal not reflected in cmake files

--- cmake/_Headers.cmake
+++ cmake/_Headers.cmake
@@ -5,15 +5,5 @@ if( MULLE_TRACE_INCLUDE)
    MESSAGE( STATUS "# Include \"${CMAKE_CURRENT_LIST_FILE}\"" )
 endif()
 
-set( INCLUDE_DIRS
-src
-)
-
-set( PRIVATE_HEADERS
-src/include-private.h
-)
-
-set( PUBLIC_HEADERS
-src/include.h
-)
+# no headers
 
